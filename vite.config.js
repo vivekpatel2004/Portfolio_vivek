14f50f731428a42f9e11da1b26474b393f56f6f8
vivek4591 <155885616+vivekpatel2004@users.noreply.github.com>
2024-09-20 17:38:14 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Portfolio/",
+  base:'/Portfolio_vivek/',
   server:{
     proxy:{
       '/api':'https://portfolio-sr2z.onrender.com',
